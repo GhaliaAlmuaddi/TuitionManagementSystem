eb8a5460beb5dc28b123306655d0f9a3c96123f2
Ghalia Al Muaddi <ghaliaalmuaddi@gmail.com>
2024-02-18 12:55:43 +0300

    dependencies

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,24 @@
-import Vapor
 
+import Vapor
+import Fluent
+import FluentPostgresDriver
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    
+//    app.databases.use(.postgres(configuration:
+//         SQLPostgresConfiguration(hostname: "localhost", username: "postgres", password: "",
+//                                  database: "plantsdb" ,
+//                                  tls: .prefer(try .init(configuration: .clientDefault)))) , as : .psql)
+//                        
+//    try app.register(collection: file())
+//    try routes(app)
+//    app.migrations.add(MyMigration())
+    
+    
+    
     // register routes
     try app.register(collection: Enrolment())
     try app.register(collection: Learner())
